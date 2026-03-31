e6971a2904436118f31ce76cfeabbb7a56c113f5
Udhaya Nithi <udhaya.24mip10043@vitbhopal.ac.in>
2026-03-31 20:53:24 +0530

    Update script4_log_analyzer.sh

--- script4_log_analyzer.sh
+++ script4_log_analyzer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 4: Log File Analyzer
-# Author: Rajkumar S (24BCE10447) | Course: Open Source Software
+# Author: Udhaya Nithi R (24MIP10043) | Course: Open Source Software
 
 LOGFILE=$1
 KEYWORD=${2:-"error"}   # Default keyword = error
